91db5a64ed5d26f7eb469f525f6fa14c577b90c6
kevinvegah <kevin.vega.h@hotmail.com>
2018-04-11 14:43:10 -0500

    Initial commit

---
+++ app/src/main/java/com/example/ceisutb14/parcialkotlin/Calculos.kt
@@ -0,0 +1,7 @@
+package com.example.ceisutb14.parcialkotlin
+
+/**
+ * Created by ceisutb14 on 11/04/18.
+ */
+class Calculos {
+}
---
+++ app/src/main/java/com/example/ceisutb14/parcialkotlin/MainActivity.kt
@@ -0,0 +1,76 @@
+package com.example.ceisutb14.parcialkotlin
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.ArrayAdapter
+import android.widget.EditText
+import android.widget.Spinner
+import kotlinx.android.synthetic.main.activity_main.*
+
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val datos = arrayOf("binario", "octal", "hexadecimal", "Decimal") // <-- Datos del Spinner
+
+
+        val sourceSpinner = findViewById<Spinner>(R.id.sourceSystem)   // <-- llamamos el Spinner SourceSystem
+        val  destiSpinner = findViewById<Spinner>(R.id.destiSystem)     // <-- llamamos el Spinner Destination System
+        var boxnumber = findViewById<EditText>(R.id.boxNumber)    // <-- llamamos el numero de entrada a convertir
+
+        val adapter = ArrayAdapter(this, android.R.layout.simple_spinner_item, datos) // <-- Ingresamos los datos en los Spinner
+        sourceSpinner.adapter = adapter
+        destiSpinner.adapter = adapter
+
+        val source = findViewById<Spinner>(R.id.sourceSystem)
+        val  destination = findViewById<Spinner>(R.id.sourceSystem)
+
+        val valSource = source.selectedItem.toString()
+        val valdestination = destination.selectedItem.toString()
+
+        if (valSource == "binario" && valdestination == "octal"){
+
+            val l = java.lang.Long.parseLong(boxnumber.toString(), 2)
+            println("HERE!!!!! "+java.lang.Long.toOctalString(l))
+
+        }else if(valSource == "octal" && valdestination == "binario"){
+
+
+        }else if(valSource == "binario" && valdestination == "hexadecimal"){
+
+
+
+
+        }else if(valSource == "hexadecimal" && valdestination == "binario"){
+
+
+
+        }else if(valSource == "binario" && valdestination == "decimal"){
+
+
+
+
+        }else if(valSource == "decimal" && valdestination == "binario"){
+
+
+
+
+        }else if(valSource == "octal" && valdestination == "hexadecimal"){
+
+
+
+
+        }else if(valSource == "hexadecimal" && valdestination == "octal"){
+
+
+
+
+        }
+
+
+
+    }
+}
